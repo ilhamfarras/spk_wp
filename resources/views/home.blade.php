5b4877881201a475f8484bb18dba81c7375766de
ilhamfarras <ilhamfarras341@gmail.com>
2024-07-12 15:21:15 +0700

    pertama commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.admin')
+
+@section('main-content')
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">{{ __('Dashboard') }}</h1>
+
+    @if (session('success'))
+    <div class="alert alert-success border-left-success alert-dismissible fade show" role="alert">
+        {{ session('success') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    @endif
+
+    @if (session('status'))
+        <div class="alert alert-success border-left-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
+
+    <div class="row">
+
+        <!-- Content Column -->
+        <div class="col-lg-6 mb-4">
+
+            <!-- Project Card Example -->
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Jantung Kiriku</h6>
+                </div>
+                <div class="card-body">
+                    <img src="{{ asset('img/2s.jpg') }}" alt="" class="img w-100">
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
